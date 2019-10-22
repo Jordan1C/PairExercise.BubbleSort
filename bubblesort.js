c1f527e7325b2fe58484053344c5e9cb1d8cfc00
Jordan Contract <jordan59@optonline.net>
2019-10-22 12:20:05 -0400

    actual bs function

--- bubblesort.js
+++ bubblesort.js
@@ -1,39 +1,43 @@
-// function bubbleSort(array) {
-//   let swapCounter = 0;
-//   let swapper = () => {
-//     for (let i = 0; i < array.length; i++) {
-//       if (array[i] > array[i + 1]) {
-//         let oldElement = array[i];
-//         array[i] = array[i + 1];
-//         array[i + 1] = oldElement;
-//         swapCounter++;
-//       }
-//     }
-//   };
-//   swapper(array);
-//   if (swapCounter === 0) {
-//     return array;
-//   } else {
-//     return bubbleSort(array);
-//   }
-// }
+let masterSwapCounter = 0;
 
 function bubbleSort(array) {
-  let swapped = false;
+  let swapCounter = 0;
   let swapper = () => {
     for (let i = 0; i < array.length; i++) {
       if (array[i] > array[i + 1]) {
         let oldElement = array[i];
         array[i] = array[i + 1];
         array[i + 1] = oldElement;
-        swapped = true;
+        swapCounter++;
       }
     }
   };
   swapper(array);
-  if (!swapped) {
+  if (swapCounter === 0) {
+    masterSwapCounter = 0;
     return array;
   } else {
+    masterSwapCounter += swapCounter;
     return bubbleSort(array);
   }
 }
+
+// function bubbleSort(array) {
+//   let swapped = false;
+//   let swapper = () => {
+//     for (let i = 0; i < array.length; i++) {
+//       if (array[i] > array[i + 1]) {
+//         let oldElement = array[i];
+//         array[i] = array[i + 1];
+//         array[i + 1] = oldElement;
+//         swapped = true;
+//       }
+//     }
+//   };
+//   swapper(array);
+//   if (!swapped) {
+//     return array;
+//   } else {
+//     return bubbleSort(array);
+//   }
+// }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,11 +1,11 @@
 describe("Bubble Sort", function() {
-  // beforeAll(function () {
-  //     spyOn(tootsiepop, 'lick').and.callThrough(); // replace existing `tootsiepop['lick']` method
-  //   });
-  //   it('getting to the center of tootsiepop involves exactly three licks', function () {
-  //     tootsiepop.getToCenter();
-  //     expect(tootsiepop.lick.calls.count()).toEqual(3);
-  //   });
+  beforeAll(function() {
+    spyOn(bubbleSort, "swapper").and.callThrough(); // replace existing `tootsiepop['lick']` method
+  });
+  it("getting to the center of tootsiepop involves exactly three licks", function() {
+    bubbleSort.getToCenter();
+    expect(tootsiepop.lick.calls.count()).toEqual(3);
+  });
   it("handles an empty array", function() {
     expect(bubbleSort([])).toEqual([]);
   });
