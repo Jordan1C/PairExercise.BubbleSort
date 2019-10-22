feaca0e58618fb9b9afd8c37162b4ffbd6e5ba8d
Jordan Contract <jordan59@optonline.net>
2019-10-22 12:19:24 -0400

    completed bs function

---
+++ bubblesort.js
@@ -0,0 +1,39 @@
+// function bubbleSort(array) {
+//   let swapCounter = 0;
+//   let swapper = () => {
+//     for (let i = 0; i < array.length; i++) {
+//       if (array[i] > array[i + 1]) {
+//         let oldElement = array[i];
+//         array[i] = array[i + 1];
+//         array[i + 1] = oldElement;
+//         swapCounter++;
+//       }
+//     }
+//   };
+//   swapper(array);
+//   if (swapCounter === 0) {
+//     return array;
+//   } else {
+//     return bubbleSort(array);
+//   }
+// }
+
+function bubbleSort(array) {
+  let swapped = false;
+  let swapper = () => {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > array[i + 1]) {
+        let oldElement = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = oldElement;
+        swapped = true;
+      }
+    }
+  };
+  swapper(array);
+  if (!swapped) {
+    return array;
+  } else {
+    return bubbleSort(array);
+  }
+}
---
+++ bubblesort.spec.js
@@ -0,0 +1,23 @@
+describe("Bubble Sort", function() {
+  // beforeAll(function () {
+  //     spyOn(tootsiepop, 'lick').and.callThrough(); // replace existing `tootsiepop['lick']` method
+  //   });
+  //   it('getting to the center of tootsiepop involves exactly three licks', function () {
+  //     tootsiepop.getToCenter();
+  //     expect(tootsiepop.lick.calls.count()).toEqual(3);
+  //   });
+  it("handles an empty array", function() {
+    expect(bubbleSort([])).toEqual([]);
+  });
+  it("handles array with only one element", function() {
+    expect(bubbleSort([1])).toEqual([1]);
+    expect(bubbleSort([2])).toEqual([2]);
+    expect(bubbleSort([5])).toEqual([5]);
+  });
+  it("sorts an array with multiple elements", function() {
+    expect(bubbleSort([3, 2])).toEqual([2, 3]);
+    expect(bubbleSort([4, 5])).toEqual([4, 5]);
+    expect(bubbleSort([16, 256, 4])).toEqual([4, 16, 256]);
+    expect(bubbleSort([75, 84, 37, 25])).toEqual([25, 37, 75, 84]);
+  });
+});
